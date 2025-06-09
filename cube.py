1f0a9b72c0a8cc71fb5b6e293b4fe5191ffd40a3
Caio <dandinzin@gmail.com>
2025-06-09 13:23:11 -0300

    exibindo dados

--- cube.py
+++ cube.py
@@ -1,6 +1,7 @@
 import pycuber as pc
 from collections import deque
 import random
+import time
 
 MOVES = ['U', "U'", 'D', "D'", 'R', "R'", 'L', "L'", 'F', "F'", 'B', "B'"]
 
@@ -8,24 +9,41 @@ def bfs_solver(scrambled_cube):
     visited = set()
     queue = deque()
     queue.append((scrambled_cube.copy(), []))  
+    start_time = time.time()
+    max_queue_size = 1
+    nodes = 0
+    total_branches = 0
+    branching_points = 0
 
     while queue:
         current_cube, path = queue.popleft()
         state_str = str(current_cube)
+        max_queue_size = max(max_queue_size, len(queue))
 
         if state_str in visited:
             continue
         visited.add(state_str)
+        nodes += 1
 
         if current_cube == pc.Cube():
-            return path
+            end_time = time.time()
+            time_for_resolve = end_time - start_time
+            avg_branching = total_branches / branching_points if branching_points else 0
+            return path, time_for_resolve, max_queue_size, nodes, avg_branching
 
+        children = 0
         for move in MOVES:
             new_cube = current_cube.copy()
             new_cube.perform_algo(move)
             queue.append((new_cube, path + [move]))
+            children += 1
+        
+        total_branches += children
+        branching_points += 1
 
-    return None
+
+    avg_branching = total_branches / branching_points if branching_points else 0
+    return None, None, max_queue_size, nodes, avg_branching
 
 def generate_scrambled_cube(n_moves):
     cube = pc.Cube()
@@ -42,8 +60,12 @@ for n in range(1, MAX_MOVES + 1):
     print(scrambled_cube)
     print(f'Movimentos usados no embaralhamento: {scramble_seq}')
 
-    solution = bfs_solver(scrambled_cube)
+    solution, time_for_resolve, max_queue_size, nodes, avg_branching = bfs_solver(scrambled_cube)
     if solution:
         print(f'Solução encontrada em {len(solution)} passos: {solution}')
+        print(f'Tempo gasto: {time_for_resolve:.4f} segundos')
+        print(f'Tamanho máximo da fila: {max_queue_size}')
+        print(f'Quantidade de nós expandidos: {nodes}')
+        print(f'Fator de ramificação média: {avg_branching}')
     else:
         print('Solução não encontrada.')
